0aa79895fbf61228cfa0ff00c9ece59fa401558d
timothymulatya <timothymulatya@gmail.com>
2025-11-21 23:02:57 +0300

    submissions

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,12 @@
 import React from "react";
-import { image } from "../data/data";
+import { image } from "../data/data.js";
 
 function About() {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>
-        I enjoy building creative solutions and learning new technologies.
-      </p>
-        <img src={require("./demo.png")} alt="I made this" />
+      <p>I made this</p>
+      <img src={image} alt="I made this" />
     </div>
   );
 }
--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import NavBar from "./components/NavBar";
-import Home from "./components/Home";
-import About from "./components/About";
+import NavBar from "./NavBar";
+import Home from "./Home";
+import About from "./About";
 
 function App() {
   return (
