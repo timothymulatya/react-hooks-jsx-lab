277ba67f7cb67d239b720f8e397c8a42bbc278b7
timothymulatya <timothymulatya@gmail.com>
2025-11-21 22:26:41 +0300

    homejsx

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,13 @@ import React from "react";
 import { name, city } from "../data/data.js";
 
 function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+    <div id="home">
+      <h1 sytle={{ color: "firebrick" }}>
+        {name} is a Web Developer from {city}
+      </h1>
+    </div>
+  );
 }
 
 export default Home;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,13 @@
 import React from "react";
 
 function NavBar() {
-  // update the JSX being returned!
-
-  return (
+   return (
   <nav>
     
     <a href="#home">Home</a>
     <a href="#about">About</a>
-    </nav>;
-  )
+    </nav>
+  );
 }
 
 export default NavBar;
