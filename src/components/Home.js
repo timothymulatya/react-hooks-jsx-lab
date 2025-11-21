71203d0ea670303860e8339ec0a21c8773316ecf
timothymulatya <timothymulatya@gmail.com>
2025-11-21 23:12:43 +0300

    home page adjsutments

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import { name, city } from "../data/data.js";
 function Home() {
   return (
     <div id="home">
-      <h1 sytle={{ color: "firebrick" }}>
+      <h1 style={{ color: "firebrick" }}>
         {name} is a Web Developer from {city}
       </h1>
     </div>
