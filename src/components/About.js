c167fb8adfa991b3b22ace028303f16673b2f9b7
timothymulatya <timothymulatya@gmail.com>
2025-11-21 22:44:28 +0300

    Appjs updates

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,15 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>
+        I enjoy building creative solutions and learning new technologies.
+      </p>
+        <img src={require("./demo.png")} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
